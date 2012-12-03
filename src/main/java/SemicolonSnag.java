7d4584d8110567270379302c26055aaa0963e858
Eirik Meland <eirikm@conduct.no>
2012-12-03 19:22:29 +0100

    initial commit

---
+++ src/main/java/SemicolonSnag.java
@@ -0,0 +1,10 @@
+public class SemicolonSnag {
+    private static final boolean DEBUG = false;
+
+    public static void main(String[] args) {
+        if (DEBUG) ;
+        {
+            System.out.println("true / false ?");
+        }
+    }
+}
